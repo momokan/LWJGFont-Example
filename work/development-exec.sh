7f7df597054977152c8c9b1cdabd6dce2db0bfb0
momokan <momokan@chocolapod.sakura.ne.jp>
2014-03-01 17:24:27 +0900

    作業用 work ディレクトリを追加

---
+++ work/development-exec.sh
@@ -0,0 +1,8 @@
+VERSION=1.1-SNAPSHOT
+
+java -jar ~/.m2/repository/net/chocolapod/lwjgfont/${VERSION}/lwjgfont-${VERSION}.jar \
+     ../materials/migu-1p-regular.ttf:28 \
+     ../materials/migu-1p-regular.ttf:45 \
+	 ../materials/TKaisho-GT01.ttc:32 \
+	 ../materials/DragonQuestFC.ttf:28 \
+	 "VLGothic@VL ゴシック:12"
